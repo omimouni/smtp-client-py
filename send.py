f6c66f48dda3a1d043ea842f195dad5420518a29
Oumar Mimouni <oumar.mimouni@gmail.com>
2022-03-13 01:18:08 +0100

    added sleep

--- send.py
+++ send.py
@@ -1,28 +1,41 @@
 #!/usr/bin/env python3
 
 import smtplib
+from time import sleep
+
+host = "smtp.mailtrap.io"
+port = 2525
+username = "8866c5d5021bb6"
+password = "8da6c5dc7fa0cf"
 
 sender = "Oumar Mimouni <oumar@noops.ma>"
-receiver = "Test User <test2@gmail.com>"
+receiver = [
+  "test2@gmail.com",
+  "test3@gmail.com",
+  "test4@gmail.com",
+  ]
 
 subject = "Hello World"
 message = "Hello World it's me"
 
-body = f"""\
-Subject: {subject}
-To: {receiver}
-From: {sender}
 
-{message}"""
+mysmtp = None
+body = """\
+Subject: %s
+Content-type: text/html
+MIME-Version: 1.0
+To: %s
+From: %s
 
-# with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
-#   server.login("8866c5d5021bb6", "8da6c5dc7fa0cf")
-#   server.sendmail(sender, receiver, message)
+%s"""
 
 try:
-  mysmtp = smtplib.SMTP("smtp.mailtrap.io", 2525)
-  mysmtp.login("8866c5d5021bb6", "8da6c5dc7fa0cf")
-
-  mysmtp.sendmail(sender, receiver, body)
+  mysmtp = smtplib.SMTP(host, port)
+  mysmtp.login(username, password)
 except Exception as e:
   print(e)
+
+for i in range(len(receiver)):
+  mysmtp.sendmail(sender, receiver[i], body % (subject, receiver[i], sender, message))
+  print("sent to: " + receiver[i])
+  sleep(.2)
